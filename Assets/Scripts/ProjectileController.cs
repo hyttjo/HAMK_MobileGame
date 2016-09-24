415e0855f38397339c2dee730f622c08e46ea3a1
Mikko Kuula <mikko.kuula@student.hamk.fi>
2016-09-24 06:26:52 +0300

    Added picking up fireball from a red box and changed layer setup

---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+    private CharacterControl cControl;
+    private Rigidbody2D rb;
+
+    public float speedX;
+    public float speedY;
+    public float decayTime;
+    private float aliveTimer;
+
+	// Use this for initialization
+	void Start () {
+
+
+        rb = GetComponent<Rigidbody2D>();
+        cControl = GameObject.Find("Player").GetComponent<CharacterControl>();
+
+        rb.AddForce(transform.up * speedY, ForceMode2D.Impulse);
+        rb.AddForce(transform.right * speedX * cControl.GetFacingDir(), ForceMode2D.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        aliveTimer += Time.deltaTime;
+        if (aliveTimer > decayTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
