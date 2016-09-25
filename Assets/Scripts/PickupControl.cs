9678470011d670af9c9ef56b95650d581ed41479
Jouni Hyttinen <jouni.ilmari.hyttinen@gmail.com>
2016-09-25 13:46:57 +0300

    Changed syntax to match master repo

---
+++ Assets/Scripts/PickupControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PickupControl : MonoBehaviour {
+    /*
+     * Tämä scripti-tiedosto käsittelee Pickup-objekteihin liittyviä toimintoja
+     * Esimerkiksi mitä tapahtuu kun pelaaja koskee PickupBoxFire-laatikkoon? (syntyy poimittava PickupFire)
+     * Entäpä mitä tapahtuu kun PickupFire syntyy? (Se lentää ilmaan)
+     * Mitä jos pelaaja nappaa PickupFire-objektin? (Pelaaja saa sen sisältämän "taikavoiman" itselleen)
+     */
+    private CharacterControl cControl;
+    public GameObject pickup;
+
+    void Start() {
+        if (gameObject.tag == "PickupFire") { //Jos tämä scripti koskee PickupFireä...
+            Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+
+            if (rBody != null) {
+                //rBody.AddForce(transform.up * Random.Range(5.75f, 11.25f)); //PickupFire lentää random suuntaan random nopeudella
+                rBody.AddForce(transform.up * 6, ForceMode2D.Impulse);
+            }
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") { //Jos pelaaja osuu pickupiin
+            cControl = col.gameObject.GetComponent<CharacterControl>(); //Tämä poimii viittauksen pelaajan CharacterControl -scriptiin
+
+            if (gameObject.tag == "PickupBoxFire") { //Jos osuttava pickup onkin laatikko
+                SpawnFire();
+            }
+
+            if (gameObject.tag == "PickupFire") { //Jos osuttava pickup onkin laatikko
+                GainFire();
+            }
+        }
+    }
+
+    void SpawnFire() {
+        //Tähän voisi kehitellä animaation alkamisen (laatikko hajoaa)
+        Destroy(gameObject); //Laatikko häviää
+        Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y);
+        Instantiate(pickup, spawnPosition, Quaternion.identity); //Syntyy poimittava power-up
+    }
+
+    void GainFire() {
+        Destroy(gameObject); //Powerup häviää
+        cControl.EnableFire(); //Pelaaja saa taikavoiman
+    }
+}
---
+++ Assets/Scripts/ProjectileControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class ProjectileControl : MonoBehaviour {
+
+    private CharacterControl cControl;
+    private Rigidbody2D rBody;
+
+    public float speedX;
+    public float speedY;
+    public float decayTime;
+    private float aliveTimer;
+
+    void Start() {
+        rBody = GetComponent<Rigidbody2D>();
+        cControl = GameObject.Find("Player").GetComponent<CharacterControl>();
+
+        if (rBody != null) {
+            rBody.AddForce(transform.up * speedY, ForceMode2D.Impulse);
+            rBody.AddForce(transform.right * speedX * cControl.GetFacingDir(), ForceMode2D.Impulse);
+        }
+    }
+
+    void Update() {
+        CheckDecay();
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Enemy") {
+            Destroy(gameObject);
+        }
+    }
+
+    void LateUpdate() {
+        Rotate();
+    }
+
+    void CheckDecay() {
+        aliveTimer += Time.deltaTime;
+
+        if (aliveTimer > decayTime) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Rotate() {
+        if (rBody != null) {
+            Vector2 moveDirection = rBody.velocity;
+
+            if (moveDirection != Vector2.zero) {
+                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+        }
+    }
+}
