5c89ef8ae486ed7bd8d504057f6e90c381867ecc
Jouni Hyttinen <jouni.ilmari.hyttinen@gmail.com>
2016-10-04 16:39:17 +0300

    Added basic GameManager. It keeps variables and GameStates stored between Scenes. Intro Scene starts it.

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public enum GameState { Intro, MainMenu, GameOver, Paused, NextLevel, Playing }
+
+public class GameManager : MonoBehaviour {
+    public GameState gameState { get; private set; }
+
+    public string[] levels = new string[] { "LevelEditor" };
+    public int level_index = 0;
+
+    public Scene scene;
+
+	private static GameManager manager = null;
+	public static GameManager Manager {
+		get { return manager; }
+	}
+ 
+	void Awake() {
+		GetThisGameManager();
+	}
+	
+	void Update () {
+        scene = SceneManager.GetActiveScene();
+
+	    switch (gameState) {
+            case GameState.Intro:
+                break;
+
+            case GameState.MainMenu:
+                if (scene.name != "MainMenu") {
+                    SceneManager.LoadScene("MainMenu");
+                }
+                break;
+
+            case GameState.GameOver:
+                break;
+
+            case GameState.Paused:
+                break;
+
+            case GameState.NextLevel:
+                if (levels != null && levels.Length > 0) {
+                    string level = levels[level_index];
+
+                    if (scene.name != level) {
+                        SceneManager.LoadScene(levels[level_index]);
+                        level_index++;
+                    }
+                }
+                SetGameState(GameState.Playing);
+                break;
+
+            case GameState.Playing:
+                break;
+        }
+	}
+ 
+	void GetThisGameManager() {
+		if (manager != null && manager != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			manager = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+    public void SetGameState(GameState state) {
+        this.gameState = state;
+    }
+}
---
+++ Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    GameManager GM;
+
+    private Texture2D logo;
+    private Texture2D mainmenu;
+
+    private Texture2D skyboxSky;
+    private Texture2D skyboxOcean;
+    private Texture2D skyboxGrass;
+    private Texture2D skyboxClouds;
+
+    void Awake () {
+        GM = FindObjectOfType<GameManager>();
+
+        logo = (Texture2D)Resources.Load("Textures/MobileGame");
+        mainmenu = (Texture2D)Resources.Load("Textures/MainMenu");
+
+        skyboxSky = (Texture2D)Resources.Load("Textures/Skybox_sky");
+        skyboxOcean = (Texture2D)Resources.Load("Textures/Skybox_ocean");
+        skyboxGrass = (Texture2D)Resources.Load("Textures/Skybox_grass");
+        skyboxClouds = (Texture2D)Resources.Load("Textures/Skybox_clouds");
+    }
+
+    public void OnGUI(){
+        if (skyboxSky != null) {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), skyboxSky);
+        }
+        if (skyboxOcean != null) {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), skyboxOcean);
+        }
+        if (skyboxGrass != null) {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), skyboxGrass);
+        }
+        if (skyboxClouds != null) {
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), skyboxClouds);
+        }
+
+        if (logo != null) {
+            GUI.DrawTexture(new Rect(Screen.width / 2 - logo.width / 2, Screen.height / 2 - 250, logo.width, logo.height), logo);
+        }
+
+        GUI.BeginGroup (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 800));
+            if (mainmenu != null) {
+                GUI.Box(new Rect (10, 0, 180, 32), mainmenu);
+            }
+            if (GUI.Button (new Rect (10, 40, 180, 40), "Start")) {
+                StartGame();
+            }
+            if (GUI.Button (new Rect (10, 120, 180, 40), "Quit")) {
+                Quit();
+            }
+        GUI.EndGroup();
+    }
+
+    public void StartGame() {
+        GM.SetGameState(GameState.NextLevel);
+    }
+
+    public void Quit() {
+        Debug.Log("Apllication Quit!");
+        Application.Quit();
+    }
+}
