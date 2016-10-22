71ec21797eb2d92219bb3f52e26ea9b0db1982be
Mikko Kuula <mikko.kuula@student.hamk.fi>
2016-10-22 15:03:57 +0300

    Created AudioControl script to handle audio events

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Delegate:
+public delegate void OnAudioEvent(Transform eventTransform);
+
+public class AudioControl : MonoBehaviour {
+
+    //Eventit:
+    public static event OnAudioEvent onPlayerJump;
+    public static event OnAudioEvent onPlayerCollectCoin;
+
+    //Metodit:
+    public static void PlayerJump(Transform eventTransform) {
+        if (onPlayerJump != null) {
+            onPlayerJump(eventTransform);
+        }
+    }
+
+    public static void PlayerCollectCoin(Transform eventTransform) {
+        if (onPlayerCollectCoin != null) {
+            onPlayerCollectCoin(eventTransform);
+        }
+    }
+}
