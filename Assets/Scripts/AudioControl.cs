d19d226c95427cb695fd54f5a819dc88ccd0d252
Mikko Kuula <mikko.kuula@student.hamk.fi>
2016-10-23 01:12:21 +0300

    PowerUp and Heart pickups have sounds

--- Assets/Scripts/AudioControl.cs
+++ Assets/Scripts/AudioControl.cs
@@ -7,12 +7,16 @@ public class AudioControl : MonoBehaviour {
 
     public AudioClip jumpSound;
     public AudioClip coinSound;
+    public AudioClip heartSound;
+    public AudioClip pickPowerUpSound;
 
     void Start() {
         audioPlayer = gameObject.AddComponent<AudioSource>();
 
         AudioControl.onPlayerJump += PlayPlayerJumpSound;
         AudioControl.onPlayerCollectCoin += PlayPlayerCollectCoinSound;
+        AudioControl.onPlayerCollectHeart += PlayPlayerCollectHeartSound;
+        AudioControl.onPlayerCollectPowerUp += PlayPlayerCollectPowerUpSound;
     }
 
     //Delegate:
@@ -21,6 +25,8 @@ public class AudioControl : MonoBehaviour {
     //Eventtien linkitys:
     public static event OnAudioEvent onPlayerJump;
     public static event OnAudioEvent onPlayerCollectCoin;
+    public static event OnAudioEvent onPlayerCollectHeart;
+    public static event OnAudioEvent onPlayerCollectPowerUp;
 
     public static void PlayerJump(GameObject unit) {
         if (onPlayerJump != null) {
@@ -34,6 +40,18 @@ public class AudioControl : MonoBehaviour {
         }
     }
 
+    public static void PlayerCollectHeart(GameObject unit) {
+        if (onPlayerCollectHeart != null) {
+            onPlayerCollectHeart(unit);
+        }
+    }
+
+    public static void PlayerCollectPowerUp(GameObject unit) {
+        if (onPlayerCollectPowerUp != null) {
+            onPlayerCollectPowerUp(unit);
+        }
+    }
+
     //Metodit jotka toistavat äänet
     public void PlayPlayerJumpSound(GameObject unit) {
         if (audioPlayer != null) {
@@ -44,6 +62,18 @@ public class AudioControl : MonoBehaviour {
     public void PlayPlayerCollectCoinSound(GameObject unit) {
         if (audioPlayer != null) {
             audioPlayer.PlayOneShot(coinSound, 0.65f);
+        }
+    }
+
+    public void PlayPlayerCollectHeartSound(GameObject unit) {
+        if (audioPlayer != null) {
+            audioPlayer.PlayOneShot(heartSound, 0.65f);
+        }
+    }
+
+    public void PlayPlayerCollectPowerUpSound(GameObject unit) {
+        if (audioPlayer != null) {
+            audioPlayer.PlayOneShot(pickPowerUpSound, 0.65f);
         }
     }
 }
